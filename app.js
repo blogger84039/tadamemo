483d8e2ef69f983f7dffe8b7b47a4f801c041f14
blogger84039 <140225811+blogger84039@users.noreply.github.com>
2024-03-04 17:47:05 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const text_0 = document.getElementById('text_01');
-const text_2 = document.getElementById('text_02')
-const title = document.getElementById('text_');
-const content = document.getElementById('area_')
-const btnclk = document.getElementById('btn_');
+const TEXT_0 = document.getElementById('text_01');
+const TEXT_2 = document.getElementById('text_02')
+const TITLE = document.getElementById('text_');
+const CONTENT = document.getElementById('area_')
+const BTNCLK = document.getElementById('btn_');
 
-btnclk.addEventListener('click', () => {
-    text_0.innerHTML = title.value;
-    title.value = '';
-    text_2.innerHTML = content.value;
-    content.value = '';
+BTNCLK.addEventListener('click', () => {
+    TEXT_0.innerHTML = TITLE.value;
+    TITLE.value = '';
+    TEXT_2.innerHTML = CONTENT.value;
+    CONTENT.value = '';
 })
